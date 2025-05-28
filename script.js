aa1c3a36dc7f6d1a7d660ed873daa6607a5d3009
farhannwo <157916953+farhannwo@users.noreply.github.com>
2025-05-28 16:54:23 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+// Smooth scroll for navigation links
+document.querySelectorAll('nav a').forEach(link => {
+    link.addEventListener('click', function (e) {
+        const href = this.getAttribute('href');
+        if (href.startsWith('#')) {
+            e.preventDefault();
+            document.querySelector(href).scrollIntoView({ behavior: 'smooth' });
+        }
+    });
+});
